ed7fa13c6afc38dbf0bd8e7b945c80bed9e124b1
Liviu Gheorghe <liviu.ionut64@yahoo.com>
2020-07-22 09:24:33 +0300

    Tried to implement system notification but tray icon not showing up

--- src/main/java/util/NotificationManager.java
+++ src/main/java/util/NotificationManager.java
@@ -1,10 +1,39 @@
 package util;
-
+import javax.imageio.ImageIO;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 
 public class NotificationManager {
-    public static void showNotification(String message) throws AWTException {
+    public static void showNotification(String message) throws AWTException, IOException {
+        if(SystemTray.isSupported()) {
+            SystemTray systemTray = SystemTray.getSystemTray();
+            String imagePath = System.getProperty("user.home")+"\\"+"smartphone.png";
+            System.out.println("Image path is : "+ imagePath);
+            Image iconImage = ImageIO.read(NotificationManager.class.getResource("/images/smartphone.png"));
+            PopupMenu popup = new PopupMenu();
+            TrayIcon trayIcon = new TrayIcon(iconImage,"PC Controller",popup);
+            trayIcon.setToolTip("Tooltip");
+            trayIcon.setImageAutoSize(true);
+            systemTray.add(trayIcon);
+        }
+        else System.out.println("System tray is not supported");
     }
 }
+
+class Driver {
+    public static void main(String[] args) {
+
+
+        System.out.println("Showing notification");
+        try {
+            NotificationManager.showNotification("Maaai");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
