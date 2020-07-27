2243a68fbedd24d0501315df3eb81cf9b094a36f
liviu-gheorghe <liviu.ionut64@yahoo.com>
2020-07-27 17:50:48 +0300

    Added static member holding FX Thread Name. Added static memeber holding a reference to the notification stage

--- src/main/java/pccontroller/App.java
+++ src/main/java/pccontroller/App.java
@@ -1,5 +1,7 @@
 package pccontroller;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
 import util.OsDetector;
 import util.XMLUserDataLoader;
 
@@ -15,8 +17,12 @@ public class App {
     public static String CONNECTED_DEVICE_NAME = "";
     public static String CONNECTED_DEVICE_IP = "";
     public static boolean CONNECTION_ACCEPTED = false;
+    public static String FX_THREAD_NAME;
+    public static Stage NOTIFICATION_STAGE;
 
     public static void onCreate() {
+        FX_THREAD_NAME = Thread.currentThread().getName();
+        System.out.printf("FX Thread name is %s", FX_THREAD_NAME);
         try {
             OS_ID = OsDetector.getMachineOsId();
         }
